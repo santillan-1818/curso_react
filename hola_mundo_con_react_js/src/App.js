8ec89e735c9811872577225109bfa402bd54031f
santillan-1818 <89090701+santillan-1818@users.noreply.github.com>
2021-08-25 11:35:59 -0500

    Agregamos hola mundo

---
+++ hola_mundo_con_react_js/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HolaMundo from './components/HolaMundo';
+
+function App() {
+  //El return solamente retorna una etiqueta
+  return (
+    <div>
+        <HolaMundo />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ hola_mundo_con_react_js/src/components/HolaMundo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const HolaMundo = () => {
+    return <h1>Hola Mundo</h1>
+}
+
+export default HolaMundo;
+
